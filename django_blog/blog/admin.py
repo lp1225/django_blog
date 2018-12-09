ec183755db03d88db141c5b79592f96c55c9a27d
lp <1402412786@qq.com>
2018-12-09 21:41:30 +0800

    admin后台的生成

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+# 注册admin后台模型
+
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    定制后台显示信息
+    """
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from . import  views
 
 
-urlpatterns = {
+urlpatterns = [
     url(r'^$', views.index, name='index')
-}
+]
